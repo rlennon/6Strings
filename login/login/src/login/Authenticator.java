484d20723f89534b2cb11a4c2c80335ec0cc3b08
Shaun Whoriskey <L00120513@student.lyit.ie>
2018-11-10 20:34:00 +0000

    Added Maven Dependency for Mongo in Java

---
+++ login/login/src/login/Authenticator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package login;
+
+/**
+ * @author whori
+ *
+ */
+public class Authenticator {
+	private String name;
+	private String pass;
+	
+	/*public Authenticator(String userIn, String passIn)
+	{
+		name = userIn;
+		pass = passIn;
+	}*/
+	
+	private boolean checkUser(String userIn, String passIn) 
+	{
+		if()
+			return true;
+		else
+			return false;
+	}
+}
